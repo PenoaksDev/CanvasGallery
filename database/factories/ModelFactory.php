2341e2c0f606070148cce9884e8899cdd0aee1d4
Risal Hidayat <risalhidayat88@gmail.com>
2017-03-23 17:07:55 +0700

    update slug value from faker generator

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -42,10 +42,10 @@ $factory->define(Canvas\Models\User::class, function (Faker\Generator $faker) {
 | Create the initial Post.
 |
 */
-$factory->define(Canvas\Models\Post::class, function () {
+$factory->define(Canvas\Models\Post::class, function (Faker\Generator $faker) {
     return [
         'title' => 'Hello World',
-        'slug' => 'hello-world',
+        'slug' => $faker->name,
         'subtitle' => 'Canvas is a simple, powerful blog publishing platform that lets you to share your stories with the world. Its beautifully designed interface allows you to create and publish your own blog, giving you tools that make it easy and even fun to do.',
         'page_image' => '/vendor/canvas/assets/images/mocha.jpg',
         'content_raw' => view('canvas::frontend.blog.partials.welcome'),
